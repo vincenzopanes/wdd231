3a83247b969e726be027836a2c044ccff31f8eb3
vincenzopanes <vpanesirribarra@byupathway.edu>
2026-07-22 18:26:25 -0400

    week03 weather

---
+++ week03/scripts/modules.mjs
@@ -0,0 +1,21 @@
+import byuiCourse from "./course.mjs";
+
+import { setSectionSelection } from "./sections.mjs";
+
+import { setTitle, renderSections } from "./output.mjs";
+
+setTitle(byuiCourse);
+setSectionSelection(byuiCourse.sections);
+renderSections(byuiCourse.sections);
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+  const sectionNum = Number(document.querySelector("#sectionNumber").value);
+  byuiCourse.changeEnrollment(sectionNum);
+  renderSections(byuiCourse.sections);
+});
+
+document.querySelector("#dropStudent").addEventListener("click", function () {
+  const sectionNum = Number(document.querySelector("#sectionNumber").value);
+  byuiCourse.changeEnrollment(sectionNum, false);
+  renderSections(byuiCourse.sections);
+});
---
+++ week03/weather/scripts/weather.js
@@ -0,0 +1,32 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=3eb99e6a72189b3aac14ec60dadce03c';
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); // uncomment when ready
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;C`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = `${desc}`;
+}
