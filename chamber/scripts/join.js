3c231e2d5328e3cd98d7a4857ba17ca2ae63957a
vincenzopanes <vpanesirribarra@byupathway.edu>
2026-08-01 20:22:13 -0400

    one more update chamber join week04

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,3 +1,23 @@
-const timestamp = document.querySelector("#timestamp");
+ const timestamp = document.querySelector("#timestamp");
 
-timestamp.value = new Date().toISOString();
+if (timestamp) {
+    timestamp.value = new Date().toISOString();
+}
+
+document.querySelectorAll(".open-modal").forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const modal = document.getElementById(link.dataset.modal);
+
+        if (modal) {
+            modal.showModal();
+        }
+    });
+});
+
+document.querySelectorAll(".close-modal").forEach(button => {
+    button.addEventListener("click", () => {
+        button.closest("dialog").close();
+    });
+});
